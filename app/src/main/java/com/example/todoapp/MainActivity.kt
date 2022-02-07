26d4cf64616fe69696492e87ce8fc096de9ce5da
Edwin Palacios <edwiinrtz@gmail.com>
2022-02-06 22:10:45 -0500

    Bussines logic implementation

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toDoAdapter: ToDoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toDoAdapter = ToDoAdapter(mutableListOf())
+
+        binding.rvTodoList.adapter = toDoAdapter
+        binding.rvTodoList.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAddNewActivity.setOnClickListener {
+            val text = binding.etNewActivity.text.toString()
+            if(text.length > 1){
+                val todo = ToDo(title = text)
+
+                toDoAdapter.addTodo(todo)
+
+            }
+            binding.etNewActivity.text.clear()
+        }
+
+
+        binding.btnDeleteActivity.setOnClickListener{
+            toDoAdapter.deleteDone()
+        }
     }
 }
---
+++ app/src/main/java/com/example/todoapp/ToDoAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.todoapp
+
+import android.graphics.Paint
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.databinding.ItemTodoBinding
+
+
+class ToDoAdapter(private val todos: MutableList<ToDo>) :
+    RecyclerView.Adapter<ToDoAdapter.TodoViewHolder>() {
+
+    fun addTodo(todo: ToDo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    fun deleteDone() {
+        todos.removeAll { todo -> todo.isChecked }
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val viewHolderInflated = LayoutInflater.from(parent.context)
+            .inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        return TodoViewHolder(viewHolderInflated)
+    }
+
+    private fun subrayar(tvItem: TextView, isChecked: Boolean) {
+        when (isChecked) {
+            true -> tvItem.paintFlags = tvItem.paintFlags or STRIKE_THRU_TEXT_FLAG
+            else -> tvItem.paintFlags = tvItem.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val actualItem = todos[position]
+        holder.itemView.apply {
+            holder.binding.tvItem.text = actualItem.title
+            holder.binding.cbDone.isChecked = actualItem.isChecked
+
+            subrayar(holder.binding.tvItem, actualItem.isChecked)
+            holder.binding.cbDone.setOnCheckedChangeListener {_,isChecked ->
+                subrayar(holder.binding.tvItem, isChecked)
+                actualItem.isChecked = !actualItem.isChecked
+            }
+
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+
+    class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        val binding = ItemTodoBinding.bind(view)
+    }
+
+}
+
